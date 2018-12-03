dd05035571f8e297768c06632926241d50836707
sdh0192 <sdh0192@live.com>
2018-12-03 18:20:01 -0500

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1",
+        ]
+    },
+
+    {
+        name: "Lou Ritter",
+        photo: "Photo Not Found",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4",
+        ]
+    },
+
+    {
+        name: "Jordan Biason",
+        photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.09/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5",
+        ]
+    }
+]
+
+module.exports = friends;
